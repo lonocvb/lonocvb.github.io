d1a14577639305381eed9340a1c36a92aaf8fe6a
Yufu Kao <lonocvb@gmail.com>
2019-11-12 14:45:13 +0000

    visual 3d label

--- art/src/app/panorama/panorama.component.ts
+++ art/src/app/panorama/panorama.component.ts
@@ -55,39 +55,18 @@ export class PanoramaComponent implements OnInit {
 
   ngAfterViewInit() {
 
-    console.log(this.pElements.map(p => p.toLabel()));
-
     this.viewer = new PanoramaViewer({
       canvas: this.canvas.nativeElement,
       imagePath: '/assets/360/04.jpg',
       width: window.innerWidth,
       height: window.innerHeight,
-/*
+
       textLabels: [
-        { text: '0,0', position: { lon: 0, lat: 0 }, args: { yo: 'yo' } },
-        { text: '45,0', position: { lon: 45, lat: 0 }, args: { yo: 'yo' } },
-        { text: '90,0', position: { lon: 90, lat: 0 }, args: { yo: 'yo' } },
-        { text: '135,0', position: { lon: 135, lat: 0 }, args: { yo: 'yo' } },
-        { text: '180,0', position: { lon: 180, lat: 0 }, args: { yo: 'yo' } },
-        { text: '225,0', position: { lon: 225, lat: 0 }, args: { yo: 'yo' } },
-        { text: '270,0', position: { lon: 270, lat: 0 }, args: { yo: 'yo' } },
-        { text: '315,0', position: { lon: 315, lat: 0 }, args: { yo: 'yo' } },
-
-        { text: '0,45', position: { lon: 0, lat: 45 }, args: { yo: 'yo' } },
-        { text: '90,45', position: { lon: 90, lat: 45 }, args: { yo: 'yo' } },
-        { text: '180,45', position: { lon: 180, lat: 45 }, args: { yo: 'yo' } },
-        { text: '270,45', position: { lon: 270, lat: 45 }, args: { yo: 'yo' } },
-
-        { text: '0,-45', position: { lon: 0, lat: -45 }, args: { yo: 'yo' } },
-        { text: '90,-45', position: { lon: 90, lat: -45 }, args: { yo: 'yo' } },
-        { text: '180,-45', position: { lon: 180, lat: -45 }, args: { yo: 'yo' } },
-        { text: '270,-45', position: { lon: 270, lat: -45 }, args: { yo: 'yo' } },
-
         { text: 'top', position: { lon: 0, lat: 90 }, args: { yo: 'yo' } },
         { text: 'bottom', position: { lon: 0, lat: -90 }, args: { yo: 'yo' } },
       ],
       onLabelClick: label => this.router.navigate(['exhibit', label.text, 'info']),
-*/
+
       elementLabels: this.pElements.map(p => p.toLabel()),
     });
 
--- art/src/app/panorama/pelement.directive.ts
+++ art/src/app/panorama/pelement.directive.ts
@@ -6,12 +6,18 @@ import { Directive, Input, ElementRef } from '@angular/core';
 export class PelementDirective {
   @Input() lon: number = 0;
   @Input() lat: number = 0;
+  @Input() opacity: number = 1;
 
   constructor(
     private el: ElementRef,
   ) {
     el.nativeElement.classList.add('p-element');
     el.nativeElement.style.position = 'absolute';
+  }
+
+  ngAfterViewInit() {
+    this.el.nativeElement.style.opacity = this.opacity;
+    this.el.nativeElement.style.transform = `scale(${Math.pow(this.opacity, 0.33) / 2.0})`;
   }
 
   toLabel() {
